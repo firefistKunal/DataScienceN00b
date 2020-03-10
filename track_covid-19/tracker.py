22c894de2e210437c3fe4ed031ad0b8ecf48109a
firefistkunal <kunal28sinha@gmail.com>
2020-03-10 12:12:57 +0530

    data entry for tracking covid-19 in india

---
+++ track_covid-19/tracker.py
@@ -0,0 +1,36 @@
+import pandas as pd 
+import csv
+from datetime import datetime as dt
+
+data=pd.read_csv('covid-19(india).csv')
+
+def date():
+
+    return dt.now().strftime("%d-%m-%Y")
+
+def time():
+    if dt.now().strftime("%p")=="AM":
+        return "Morning"
+    else:
+        return "Evening"
+
+def addInfection():
+    State=input("State??\t")
+    Place=input("Place??\t")
+    Date=date()
+    Time=time()
+    Infected=int(input("How many Infected??\t"))
+    Total_Infected=data["Total_Infected"].iloc[-1]+Infected
+
+    return [State, Place, Date, Time, Infected, Total_Infected]
+
+
+while int(input("1 to Add\n0 to close")):
+
+    print(data)
+    data.loc[len(data)]=addInfection()
+    print(data)
+    data.to_csv('covid-19(india).csv', index=False)
+    
+
+
