58476f060514f5c91d0f2811d9031312f863704e
firefistkunal <kunal28sinha@gmail.com>
2020-03-11 18:38:04 +0530

    total area under cultivation by year

--- Agriculture/agri.py
+++ Agriculture/agri.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np 
 import matplotlib as mp
 
-data=pd.read_csv(r"C:\Users\madhura.anand\Desktop\pybasic\pandasplay\agriiiiiiiiiiiii.csv")
-yr=data[['State_Name', 'Area','Crop_Year','Crop']].groupby(['State_Name','Crop'])
-for key,item in yr:
-    df=yr.get_group(key)
-    print(df, "\n\n")
-    maxyear=df['Crop_Year'].max()
-    print(maxyear)
-    # print(df.loc[('maxyear'),'Area'])
+data=pd.read_csv("apy.csv")
+# yr=data[['State_Name', 'Area','Crop_Year','Crop']].groupby(['State_Name','Crop'])
+# for key,item in yr:
+#     df=yr.get_group(key)
+#     print(df, "\n\n")
+#     maxyear=df['Crop_Year'].max()
+#     print(maxyear)
+#     # print(df.loc[('maxyear'),'Area'])
 
+yr=data[data['State_Name']=='Chhattisgarh'] #take data of a state, any state
 
+yr=yr.groupby(['Crop_Year'])['Area'].agg(np.sum) #group it by year and add by area
+print(yr) #print the output
 
