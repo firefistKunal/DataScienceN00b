ca472b7e89824f517c46cf09aaf2bf9ab7c4b83d
Madhura Anand <madhura1396@gmail.com>
2020-03-11 17:52:28 +0530

    Add files via upload

--- Agriculture/agri.py
+++ Agriculture/agri.py
@@ -5,7 +5,11 @@ import matplotlib as mp
 data=pd.read_csv(r"C:\Users\madhura.anand\Desktop\pybasic\pandasplay\agriiiiiiiiiiiii.csv")
 yr=data[['State_Name', 'Area','Crop_Year','Crop']].groupby(['State_Name','Crop'])
 for key,item in yr:
-        print(yr.get_group(key), "\n\n")
-print(".......................................................................................................................................")
-for key,item in yr:
-    print(yr.get_group(key), "\n\n")
+    df=yr.get_group(key)
+    print(df, "\n\n")
+    maxyear=df['Crop_Year'].max()
+    print(maxyear)
+    print(df.loc[('2001'),'Area'])
+
+
+
