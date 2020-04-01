8955cca9450a896e204db15118d3b6006b1c9744
Madhura Anand <madhura1396@gmail.com>
2020-04-01 18:36:22 +0530

    Add files via upload
    LR PROBLEM ON SWEDATA

--- swedatanal.py
+++ swedatanal.py
@@ -9,25 +9,17 @@ X=data['X']
 #print(X)
 Y=data['Y']
 #print(Y)
-learning_rate=0.0001
+learning_rate=0.00001
 m,b=0,0
 N=len(X)
-
-fig, (ax1, ax2,ax3) = plt.subplots(3)
-
-ax1.scatter(data['X'], data['Y'])
-ax1.set_title('X v Y distribution')
-ax1.set(xlabel='X', ylabel='Y')
 
 def update_weights(m, b, X, Y, learning_rate):
     m_deriv = 0
     b_deriv = 0
 
     for i in range(len(X)):
-        # Calculate partial derivatives
-        # -2x(y - (mx + b))
+        #  -2x(y - (mx + b))
         m_deriv += -2*X[i] * (Y[i] - (m*X[i] + b))
-
         # -2(y - (mx + b))
         b_deriv += -2*(Y[i] - (m*X[i] + b))
 
@@ -39,21 +31,15 @@ def update_weights(m, b, X, Y, learning_rate):
 m1,b1=update_weights(m, b, X, Y, learning_rate)
 #print(m1,b1)
 
-Y_pred = m1*X + b# Equation of the line we modeled
+Y_pred = m1*X + b # Equation of the line we modeled
 #print(type(Y_pred))
-ax2.scatter(data['X'], data['Y'])
-ax2.set_title('Line Obtained After Training')
-ax2.set(xlabel='X', ylabel='Y')
 for i in range(len(Y)):
     SE+=((Y_pred[i]-Y[i])**2)
     #print(SE)
     SE=SE/N
     #print(SE)
     MSE=np.sqrt(SE/N)
-print(MSE)
-# prediction_space= ([min(X), max(X)])
-#
-# ax2.plot(prediction_space, [min(Y_pred), max(Y_pred)], color='red')
+print("MEAN SQUARE ERROR IS ",MSE)
 
-#plt.show()
+
 
