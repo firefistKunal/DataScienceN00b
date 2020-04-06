47c6d3904a243e8b37ee1f9cf4136de79dd2893e
firefistkunal <kunal28sinha@gmail.com>
2020-04-06 13:17:10 +0530

    Quality of life changes, enter the total infection number it wilsubtract and add to that date

--- track_covid-19/tracker.py
+++ track_covid-19/tracker.py
@@ -1,13 +1,16 @@
 import pandas as pd 
 import numpy as np
 import csv
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta as td
 
 data=pd.read_csv('covid-19(india).csv')
 
 def date():
 
     return dt.now().strftime("%d-%m-%Y")
+# def date():
+#     day=dt.now()-td(1)
+#     return day.strftime("%d-%m-%Y")
 
 def time():
     if dt.now().strftime("%p")=="AM":
@@ -20,15 +23,21 @@ def addInfection():
     Place=input("Place??\t")
     Date=date()
     Time=time()
-    Infected=int(input("How many Infected??\t"))
+    Infected=infected(State)
     Total_Infected=data["Total_Infected"].iloc[-1]+Infected
 
     return [State, Place, Date, Time, Infected, Total_Infected]
 
+def infected(state):
+    df=data.groupby(['State'])['Infected'].agg(np.sum).reset_index()
+    state_total=df[df['State']==state].iloc[0]['Infected']
+    
+    return int(input("How many Total Infected??\t"))-state_total
+
 
 while int(input("1 to Add\n0 to close")):
     df=data.groupby(['State'])['Infected'].agg(np.sum).to_frame()
-    print(df.sort_values(by=['Infected'], ascending=False))
+    print(df.sort_values(by=['State'], ascending=False))
     data.loc[len(data)]=addInfection()
     print(data)
     data.to_csv('covid-19(india).csv', index=False)
